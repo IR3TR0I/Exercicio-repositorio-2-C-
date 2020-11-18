bf36a121713d21662de6ffa2303de7051c1974d6
Ruan <ruangustavo50711@gmail.com>
2020-11-18 16:44:13 -0300

    Exercicio 2 finalizado

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio_2_de_Repetição_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string login = "";
+            string senha = "";
+
+            while(true){
+                Console.WriteLine("Digite seu login");
+                login = Console.ReadLine();
+
+                
+                Console.WriteLine("Digite sua senha: ");
+                senha = Console.ReadLine();
+
+                if(login == senha){
+                    Console.WriteLine("Há algo de errado no login ou na senha");
+                }else{break;}
+            }
+            Console.WriteLine("Login aprovado,Bem-vindo" +login);
+        }
+    }
+}
